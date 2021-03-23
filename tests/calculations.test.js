f7a71795cab22a3964b8ad73d45e668482d242c1
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2021-03-23 11:38:51 +0300

    Added values from values.data.json

--- tests/calculations.test.js
+++ tests/calculations.test.js
@@ -1,53 +1,59 @@
 const mainPage = require('../pages/main.page');
+const values = require('../data/values.data.json')
 
 describe('Calculations', () => {
-    it('Addition of two numbers', () => {
-        mainPage.open()
-                .chooseBuild('Prototype')
-                .setFirstNumber(8)
-                .setSecondNumber(3)
-                .chooseOperation('Add')
-                .clickCalculateButton();
-        assert.equal(8+3, mainPage.getAnswer());
-    });
 
-    it('Subtraction of two numbers', () => {
-        mainPage.open()
-                .chooseBuild('Prototype')
-                .setFirstNumber(8)
-                .setSecondNumber(3)
-                .chooseOperation('Subtract')
-                .clickCalculateButton();
-        assert.equal(8-3, mainPage.getAnswer());
-    });
+    context('Results validation', () => {
+        values.forEach(values => {
+            it(`Addition of ${values.firstNumber} and ${values.secondNumber} should be equal to ${values.additionResult}`, () => {
+                mainPage.open()
+                        .chooseBuild('Prototype')
+                        .setFirstNumber(values.firstNumber)
+                        .setSecondNumber(values.secondNumber)
+                        .chooseOperation('Add')
+                        .clickCalculateButton();
+                assert.equal(values.additionResult, mainPage.getAnswer(), `Result should be ${values.additionResult}`);
+            });
 
-    it('Multiplication of two numbers', () => {
-        mainPage.open()
-                .chooseBuild('Prototype')
-                .setFirstNumber(8)
-                .setSecondNumber(3)
-                .chooseOperation('Multiply')
-                .clickCalculateButton();
-        assert.equal(8*3, mainPage.getAnswer());
-    });
+            it(`Subtraction of ${values.firstNumber} and ${values.secondNumber} should be equal to ${values.subtractionResult}`, () => {
+                mainPage.open()
+                        .chooseBuild('Prototype')
+                        .setFirstNumber(8)
+                        .setSecondNumber(3)
+                        .chooseOperation('Subtract')
+                        .clickCalculateButton();
+                assert.equal(values.subtractionResult, mainPage.getAnswer(), `Result should be ${values.subtractionResult}`);
+            });
 
-    it('Division of two numbers', () => {
-        mainPage.open()
-                .chooseBuild('Prototype')
-                .setFirstNumber(8)
-                .setSecondNumber(3)
-                .chooseOperation('Divide')
-                .clickCalculateButton();
-        assert.equal(8/3, mainPage.getAnswer());
-    });
+            it(`Multiplication of ${values.firstNumber} and ${values.secondNumber} should be equal to ${values.multiplicationResult}`, () => {
+                mainPage.open()
+                        .chooseBuild('Prototype')
+                        .setFirstNumber(8)
+                        .setSecondNumber(3)
+                        .chooseOperation('Multiply')
+                        .clickCalculateButton();
+                assert.equal(values.multiplicationResult, mainPage.getAnswer(), `Result should be ${values.multiplicationResult}`);
+            });
 
-    it('Concatenation of two numbers', () => {
-        mainPage.open()
-                .chooseBuild('Prototype')
-                .setFirstNumber(8)
-                .setSecondNumber(3)
-                .chooseOperation('Concatenate')
-                .clickCalculateButton();
-        assert.equal(83, mainPage.getAnswer());
+            it(`Division of ${values.firstNumber} and ${values.secondNumber} should be equal to ${values.divisionResult}`, () => {
+                mainPage.open()
+                        .chooseBuild('Prototype')
+                        .setFirstNumber(8)
+                        .setSecondNumber(3)
+                        .chooseOperation('Divide')
+                        .clickCalculateButton();
+                assert.equal(values.divisionResult, mainPage.getAnswer(), `Result should be ${values.divisionResult}`);
+            });
+
+            it(`Concatenation of ${values.firstNumber} and ${values.secondNumber} should be equal to ${values.concatenationResult}`, () => {
+                mainPage.open()
+                        .chooseBuild('Prototype')
+                        .setFirstNumber(8)
+                        .setSecondNumber(3)
+                        .chooseOperation('Concatenate')
+                        .clickCalculateButton();
+                assert.equal(values.concatenationResult, mainPage.getAnswer(), `Result should be ${values.concatenationResult}`);
+            });
+        });
     });
 });
